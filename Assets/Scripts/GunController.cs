f165b35df974d340e2f8da0e2e17f76070d1303e
bellflwr <70229194+bellflwr@users.noreply.github.com>
2023-11-18 18:05:54 -0500

    Added basic gun controls and crosshair.

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public LayerMask hitLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 100f, hitLayer)) {
+                Destroy(hit.transform.gameObject);
+            }
+        }
+    }
+}
