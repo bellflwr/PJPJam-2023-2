0f6d27bcd40254123ee657a6b79e0a65a3e25b72
Andrei <142443853+hjzres@users.noreply.github.com>
2023-11-18 18:53:24 -0500

    removed monobehaviour from GunInstance script

--- Assets/Scripts/GunInstance.cs
+++ Assets/Scripts/GunInstance.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GunInstance : MonoBehaviour
+public class GunInstance
 {
     public GunData gunData;
     public int loaded;
