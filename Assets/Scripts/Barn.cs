6b598b41c5cba9605d48fa495ec43ff2fd8aaceb
hjzre <142443853+hjzres@users.noreply.github.com>
2023-11-19 17:40:28 -0500

    Finished death effect

--- Assets/Scripts/Barn.cs
+++ Assets/Scripts/Barn.cs
@@ -7,8 +7,10 @@ using UnityEngine.SceneManagement;
 
 public class Barn : MonoBehaviour
 {
+    [SerializeField] GameObject barn;
     [SerializeField] float startHealth;
     [SerializeField] GameObject loseScreen;
+    [SerializeField] Animator animator;
     [SerializeField] ParticleSystem loseParticles;
 
     public float Health
@@ -24,6 +26,7 @@ public class Barn : MonoBehaviour
     public void Awake()
     {
         Health = 1000;
+        animator = loseScreen.GetComponent<Animator>();
     }
 
     void CheckAlive()
@@ -36,10 +39,11 @@ public class Barn : MonoBehaviour
 
     IEnumerator Lose(){
         loseParticles.Play();
-        yield return new WaitUntil(() => loseParticles.isStopped);
-        Destroy(gameObject);
+        yield return new WaitForSeconds(0.5f);
+        Destroy(barn);
         yield return new WaitForSeconds(0.5f);
         loseScreen.SetActive(true);
+        animator.Play("YouLoseAnimation");
         yield return new WaitForSeconds(5f);
         SceneManager.LoadScene(0);
     }
