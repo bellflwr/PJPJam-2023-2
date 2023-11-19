6adc568e14fa3573d42f449c81761698d921e184
bellflwr <70229194+bellflwr@users.noreply.github.com>
2023-11-18 22:52:05 -0500

    Man IDK

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -7,30 +7,24 @@ public class GunController : MonoBehaviour
     public LayerMask hitLayer;
     private float _lastBurst = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         GunInstance gun = StaticData.guns[StaticData.currentGun];
-        if((Input.GetMouseButtonDown(0) || Input.GetMouseButton(0) && gun.gunData.automatic) && gun.loaded > 0) {
-            if(Time.time - _lastBurst > gun.gunData.fireInterval) {
-                
+        if ((Input.GetMouseButtonDown(0) || Input.GetMouseButton(0) && gun.gunData.automatic) && gun.loaded > 0)
+        {
+            if (Time.time - _lastBurst > gun.gunData.fireInterval)
+            {
                 StartCoroutine(Burst(gun.gunData.burst, gun.gunData.burstInterval));
             }
         }
     }
 
-    IEnumerator Burst(int times, float interval) {
+    IEnumerator Burst(int times, float interval)
+    {
         _lastBurst = Time.time;
-        print("pluh");
 
         int i = 0;
-        while(i < times)
+        while (i < times)
         {
             Shoot();
             yield return new WaitForSeconds(interval); //wait 1 second per interval
@@ -38,11 +32,14 @@ public class GunController : MonoBehaviour
         }
     }
 
-    void Shoot() {
+    void Shoot()
+    {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 100f, hitLayer)) {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 100f, hitLayer))
+        {
             // Destroy(hit.transform.gameObject);
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance,
+                Color.yellow);
         }
     }
 }
