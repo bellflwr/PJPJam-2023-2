13b87e525e23d4373c9d601b390d666d5d89f6bd
Seb18456 <sebastian18678@gmail.com>
2023-11-18 20:40:06 -0500

    Created terrain & skybox

---
+++ Assets/Scripts/SkyCycle.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SkyCycle : MonoBehaviour
+{
+    [SerializeField] private Material skybox;
+
+    private void Update()
+    {
+        skybox.SetVector(name="_MainLightDirection", transform.forward);
+    }   
+}
