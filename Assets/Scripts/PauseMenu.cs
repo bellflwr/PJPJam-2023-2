3ae33d5c275289cbab390ab3163a753307411481
W1ckle <143858868+W1ckle@users.noreply.github.com>
2023-11-19 15:53:46 -0500

    Created Pause Menu and functionality
    - Game itself freezes in pause menu and unfreezes after resume
    - Options menu available
    - Can go back to main menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject menu;
+    
+    public bool Paused = false;
+
+    public void PauseGame() {
+        menu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Paused = true;
+        Time.timeScale = 0;
+        Debug.Log("Game has paused");
+    }
+    
+    public void ResumeGame() {
+        menu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Paused = false;
+        Time.timeScale = 1;
+        Debug.Log("Game has resumed");
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape) && !Paused) {
+            PauseGame();
+        }
+    }
+
+    public void GoToMainMenu() {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+}
