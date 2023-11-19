9c9253a4f34293b2018b783d1cb2ad3b96d351d3
Andrei <142443853+hjzres@users.noreply.github.com>
2023-11-18 19:56:45 -0500

    Got the ranged system working

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -12,11 +12,22 @@ public class EnemyMovement : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        agent.speed = enemyData.speed;
         agent.destination = target.position;
     }
 
     void Update()
     {
-        
+        if (enemyData.ranged)
+        {
+            if(Vector3.Distance(transform.position, target.position) <= enemyData.range)
+            {
+                agent.destination = transform.position;
+            }
+            else
+            {
+                agent.destination = target.position;
+            }
+        }
     }
 }
